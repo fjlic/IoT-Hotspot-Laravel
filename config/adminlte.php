89bda02001a1392928ff0579bf558ba423f73a26
fjlic <franc.javier.flores@gmail.com>
2021-11-03 22:51:25 -0600

    Update Config Adminlte

--- config/adminlte.php
+++ config/adminlte.php
@@ -226,21 +226,21 @@ return [
 
     'menu' => [
         // Navbar items:
-        [
+        /*[
             'type'         => 'navbar-search',
             'text'         => 'search',
             'topnav_right' => true,
-        ],
+        ],*/
         [
             'type'         => 'fullscreen-widget',
             'topnav_right' => true,
         ],
 
         // Sidebar items:
-        [
+        /*[
             'type' => 'sidebar-menu-search',
             'text' => 'search',
-        ],
+        ],*/
         [
             'text' => 'blog',
             'url'  => 'admin/blog',
