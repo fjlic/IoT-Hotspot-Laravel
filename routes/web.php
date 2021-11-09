b284175a658bc821a5ec6fe1136100dfa00ff9be
fjlic <franc.javier.flores@gmail.com>
2021-11-09 13:10:46 -0600

    update config

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {return view('auth.login');});
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
