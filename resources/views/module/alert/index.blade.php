cece8a655ce7f4b50cadf3845fdbf26d98aff6c7
fjlic <franc.javier.flores@gmail.com>
2021-12-22 01:22:51 -0600

    ..

--- resources/views/module/alert/index.blade.php
+++ resources/views/module/alert/index.blade.php
@@ -144,7 +144,7 @@
 @component('mail::subcopy')
     {{ $alert->footer }} 🔗<br/><br/>
     <https://hotspot.fjlic.com/historialsensor/chart/{{ $sensor->id }}> ✌️ <br/><br/>
-    ![QR](data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(100)->generate('https://hotspot.fjlic.com/historialsensor/chart/$sensor->id')) !!})
+    ![](data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(50)->generate('https://hotspot.fjlic.com/historialsensor/chart/{{$sensor->id}}')) !!})
 @endcomponent
 
 
@@ -152,7 +152,7 @@ Gracias, Atte. {{ config('app.name') }} 👻
 @endcomponent
 </div>
 <div>
-<img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(100)->generate('https://hotspot.fjlic.com/historialsensor/chart/$sensor->id')) !!}">
+<img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(100)->generate('https://hotspot.fjlic.com/historialsensor/chart/{{$sensor->id}}')) !!}">
 </div>
 <div class="modal-footer d-flex justify-content-center">
 <button type="button" class="btn btn-info" data-dismiss="modal">Cerrar</button>
